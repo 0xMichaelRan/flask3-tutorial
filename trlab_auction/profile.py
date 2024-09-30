ba5a4f6c413a5effb302aebf16d98ea1bc03b9a0
0xMichaelRan <michaelran16@gmail.com>
2024-10-01 02:16:35 +0800

    fix routing for /profile

--- trlab_auction/profile.py
+++ trlab_auction/profile.py
@@ -4,12 +4,12 @@ bp = flask.Blueprint("profile", __name__, url_prefix="/profile")
 
 
 @bp.route("/edit", methods=("GET", "POST"))
-def profile():
+def edit():
     return flask.render_template("profile/edit-profile.html")
 
 
 @bp.route("/settings", methods=("GET", "POST"))
-def account_settings():
+def settings():
     return flask.render_template("profile/account-settings.html")
 
 
